bde96dd9c591278a266b9e2e370f4dfb5d06821d
lee95292 <lee95292@naver.com>
2019-07-03 23:23:43 +0900

    posted_

--- deployChange.sh
+++ deployChange.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 bundle exec jekyll build
 cd _site
+
+curDate = date
+echo curDate
+
 git add .
-git commit -m "posted_ ${date}"
+git commit -m "posted_ ${curDate}"
 git push origin master
 
